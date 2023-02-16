3fe1aba1f7bcec01afec7f3b75d4db88ab622e12
acid-harry <igorkrivouhov@gmail.com>
2023-02-16 19:33:07 +0300

    Task_1 is done

---
+++ Task_1.java
@@ -0,0 +1,26 @@
+// Пусть дан произвольный список целых чисел, удалить из него четные числа (в языке уже есть что-то готовое для этого)
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Task001 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.printf("Начальный список: %s\n", list1);
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.printf("Список с удаленными числами: %s", list1);
+    }
+}
